c8417b62da17e6eafc26df7fdc42103b0057c6c1
Azat S <to@azat.io>
2023-04-25 20:49:44 +0300

    chore: init

---
+++ eslint/index.js
@@ -0,0 +1,13 @@
+let config = [
+  {
+    languageOptions: {
+      ecmaVersion: 2023,
+      sourceType: 'module',
+    },
+    rules: {
+      'no-var': 'error',
+    },
+  },
+]
+
+module.exports = config
